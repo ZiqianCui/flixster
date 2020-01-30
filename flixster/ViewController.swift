2ffbc90f4e53bacd9e2d52e328c7a1040ec30f45
ZiqianCui <zic108@ucsd.edu>
2020-01-29 20:49:48 -0800

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Ziqian Cui on 1/29/20.
+//  Copyright © 2020 Ziqian Cui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
